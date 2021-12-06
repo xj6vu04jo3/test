c3e437f2b9e8d6ced09514c28edf125a17238991
xj6vu04jo3 <95622577+xj6vu04jo3@users.noreply.github.com>
2021-12-06 20:36:37 +0800

    Update car.py

--- car.py
+++ car.py
@@ -4,19 +4,21 @@ gas = st.number_input('油量的資料收集:油箱滿是10格 =>')
 speed= st.number_input('車速的資料收集:限速100 =>')
 temp = st.number_input('溫度的資料收集:限溫30 =>')
 轉速=int(input('轉速資料的收集:限轉12000 =>'))
-if gas <= car_simulation.get('gas_warning'):
-  st.write('油箱只剩', gas, '格! 準備加油!!')
-else:
-  st.write('油箱還剩', gas, '格。')
-if speed>=car_simulation.get('speed_limit'):
-    st.write('即將超速')
-else:
+confirm_input = st.button('輸入確認')
+if confirm_input:
+  if gas <= car_simulation.get('gas_warning'):
+    st.write('油箱只剩', gas, '格! 準備加油!!')
+  else:
+    st.write('油箱還剩', gas, '格。')
+  if speed>=car_simulation.get('speed_limit'):
+     st.write('即將超速')
+  else:
     st.write('安全')
-if temp>=car_simulation.get('temp_warning'):
+  if temp>=car_simulation.get('temp_warning'):
     st.write('過熱')
-else:
+  else:
     st.write('正常')
-if 轉速>=car_simulation.get('轉速'):
+  if 轉速>=car_simulation.get('轉速'):
     st.write('即將超過轉速')
-else:
+  else:
     st.write('正常')
