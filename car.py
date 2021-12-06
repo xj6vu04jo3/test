82deca1054efe864a2386bcd73f5195e1a51eca6
xj6vu04jo3 <95622577+xj6vu04jo3@users.noreply.github.com>
2021-12-06 20:39:16 +0800

    Update car.py

--- car.py
+++ car.py
@@ -3,7 +3,7 @@ car_simulation = {'gas_warning':1, 'speed_limit':100, 'temp_warning':30, '轉速
 gas = st.number_input('油量的資料收集:油箱滿是10格 =>')
 speed= st.number_input('車速的資料收集:限速100 =>')
 temp = st.number_input('溫度的資料收集:限溫30 =>')
-轉速=int(input('轉速資料的收集:限轉12000 =>'))
+轉速=st.number_input('轉速資料的收集:限轉12000 =>')
 confirm_input = st.button('輸入確認')
 if confirm_input:
   if gas <= car_simulation.get('gas_warning'):
