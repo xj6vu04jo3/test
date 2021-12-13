12c39e9a1d5bf6ce825470c1808037c35f2103e5
xj6vu04jo3 <95622577+xj6vu04jo3@users.noreply.github.com>
2021-12-13 20:39:44 +0800

    Update test3.py

--- test3.py
+++ test3.py
@@ -15,20 +15,20 @@ def ds():
 
 def tax():
    st.write("Pd=Ps+t")
-    a=st.slider('需求線截距')
-    b=st.slider('需求線斜率(絕對值)')
-    c=st.slider('供給線截距')
-    d=st.slider('供給線斜率')
-    t=st.slider('稅'))
-    p1=(a-c)/(b+d)
-    ps=(a-c-b*t)/(b+d)
-    pd=ps+t
-    q2=a-b*pd
-    eq=(round(pd,2),round(q2,2))
-    q=round(pd-p1,2)
-    w=round((pd-p1)/t,2)
-    e=round(p1-ps,2)
-    r=round((p1-ps)/t,2)
+   a=st.slider('需求線截距')
+   b=st.slider('需求線斜率(絕對值)')
+   c=st.slider('供給線截距')
+   d=st.slider('供給線斜率')
+   t=st.slider('稅'))
+   p1=(a-c)/(b+d)
+   ps=(a-c-b*t)/(b+d)
+   pd=ps+t
+   q2=a-b*pd
+   eq=(round(pd,2),round(q2,2))
+   q=round(pd-p1,2)
+   w=round((pd-p1)/t,2)
+   e=round(p1-ps,2)
+   r=round((p1-ps)/t,2)
    st.write('稅後均衡價格與數量：')
    st.write(eq)
    st.write('轉嫁給消費者的稅：')
