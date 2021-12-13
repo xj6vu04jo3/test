3bbb1c2884b82b9f71403d3cf123fc1e4b125148
xj6vu04jo3 <95622577+xj6vu04jo3@users.noreply.github.com>
2021-12-13 19:43:45 +0800

    Update bmi.py

--- bmi.py
+++ bmi.py
@@ -9,15 +9,13 @@ def BMI(w, h):
 
 w = st.slider('請輸入體重(KG)？')
 h = st.slider('請輸入身高(M)？')
-
-
-    bmi = BMI(w, h)
-    st.write('BMI為', bmi)
-    if (bmi < 18):
-        st.write('體重過輕')
-    elif (bmi < 24):
-        st.write('體重正常')
-    elif (bmi < 27):
-        st.write('體重過重')
-    else:
-        st.write('體重肥胖')
+bmi = BMI(w, h)
+st.write('BMI為', bmi)
+if (bmi < 18):
+   st.write('體重過輕')
+elif (bmi < 24):
+   st.write('體重正常')
+elif (bmi < 27):
+   st.write('體重過重')
+else:
+   st.write('體重肥胖')
